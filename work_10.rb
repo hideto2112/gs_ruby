3098631a781a0929c9ca4d3393c26156b4ff985d
hideto2112 <e10005@shibaura-it.ac.jp>
2018-08-01 00:52:38 +0900

    gs rails講義 課題No.10

---
+++ work_10.rb
@@ -0,0 +1,16 @@
+require './common'
+url = 'https://gsacademy.tokyo/'
+charset = nil
+
+html = open(url) do |f|
+    charset = f.charset
+    f.read
+end
+
+doc = Nokogiri::HTML.parse(html, nil, charset)
+
+doc.css('p.label').each do |title|
+    if (title.text == "INTERVIEW")
+        puts title.parent.css('.title').text
+    end
+end
