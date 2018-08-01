e171cd1a42baa9aadb72fa08f5464774a3164b71
hideto2112 <e10005@shibaura-it.ac.jp>
2018-08-01 11:10:56 +0900

    gs rails講義 課題No.11

---
+++ work_11.rb
@@ -0,0 +1,16 @@
+require './common'
+url = 'https://gsacademy.tokyo/'
+charset = nil
+
+html = open(url) do |f|
+    charset = f.charset
+    f.read
+end
+
+doc = Nokogiri::HTML.parse(html, nil, charset)
+
+doc.css('a').each do |anchor|
+    if(anchor.css('.label').text == 'INTERVIEW')
+        puts anchor[:href]
+    end
+end
