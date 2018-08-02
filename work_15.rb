2dcc86c8d97fda98577fadd5e0c49d814c23a86d
hideto2112 <e10005@shibaura-it.ac.jp>
2018-08-02 13:55:39 +0900

    gs rails講義 課題No.15

---
+++ work_15.rb
@@ -0,0 +1,16 @@
+require './common'
+
+dataRange   = 1..50
+ageRange    = 0..100
+genderArray = ["male", "female"]
+
+
+dataRange.each{ |num|
+    gender = genderArray[rand(2)]
+    age = rand(ageRange)
+    User.create(
+        gender: gender,
+        age: age
+    )
+}
+
