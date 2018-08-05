fa276fd142c96afb5f1ff7680cb12626639ab906
hideto2112 <e10005@shibaura-it.ac.jp>
2018-08-05 18:34:38 +0900

    gs rails講義 課題No.06 修正

--- work_06.rb
+++ work_06.rb
@@ -1,11 +1,11 @@
 puts "あなたの手を英字1文字で入力してください（g:グー、c:チョキ、p:パー）"
 input = gets.chomp!
 
-if (input == "g")
+if input == "g"
     player = "グー"
-elsif (input == "c")
+elsif input == "c"
     player = "チョキ"
-elsif (input == "p")
+elsif input == "p"
     player = "パー"
 end
 
@@ -15,10 +15,10 @@ com = array[rand(3)]
 print "あなたは"+player+"、私は"+com+"、"
 
 # 勝敗判定
-if (player == com)
+if player == com
     puts "引き分けです。"
 else
-    if (player == "グー" && com == "チョキ" || player == "チョキ" && com == "パー" || player == "パー" && com == "グー")
+    if player == "グー" && com == "チョキ" || player == "チョキ" && com == "パー" || player == "パー" && com == "グー"
         puts "あなたの勝ちです。"
     else
         puts "あなたの負けです。"
