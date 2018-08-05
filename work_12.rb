c10b31deed1b830cc86f7ddf4eb4b17ea506116d
hideto2112 <e10005@shibaura-it.ac.jp>
2018-08-05 18:33:08 +0900

    gs rails講義 課題No.12 修正

--- work_12.rb
+++ work_12.rb
@@ -1,29 +1,28 @@
 require './common'
-url = 'https://gsacademy.tokyo/'
-charset = nil
 
-html = open(url) do |f|
-    charset = f.charset
-    f.read
+def getHtml(url, charset)
+    html = open(url) do |f|
+        charset = f.charset
+        f.read
+    end
+
+    return Nokogiri::HTML.parse(html, nil, charset)
 end
 
-doc = Nokogiri::HTML.parse(html, nil, charset)
+url = 'https://gsacademy.tokyo/'
+charset = nil
+doc = getHtml(url, charset)
 
 doc.css('a').each do |anchor|
-    if(anchor.css('.label').text == 'INTERVIEW')
+    if anchor.css('.label').text == 'INTERVIEW'
         puts ["Title：", anchor.css('.title').text]
 
         # Interview記事のリンク取得
         content_url = anchor[:href]
-
-        # リンク先取得
-        content_html = open(content_url) do |content|
-            charset = content.charset
-            content.read
-        end
-
-        # オブジェクトを作成
-        content_doc = Nokogiri::HTML.parse(content_html, nil, charset)
+        charset = nil
+        
+        # リンク先を取得し、オブジェクトを作成
+        content_doc =  getHtml(content_url, charset)
 
         puts "Article："
 
